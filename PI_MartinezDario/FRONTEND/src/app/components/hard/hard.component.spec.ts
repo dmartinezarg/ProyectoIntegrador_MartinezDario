f539d2da2f2c9351a870d6dfef69bbb2472b875b
Darío Martínez <dmartinezherfasa@gmail.com>
2023-02-25 10:47:53 -0300

    Cree la carpeta frontend

--- PI_MartinezDario/FRONTEND/src/app/components/hard/hard.component.spec.ts
+++ PI_MartinezDario/FRONTEND/src/app/components/hard/hard.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialComponent } from './social.component';
+import { HardComponent } from './hard.component';
 
-describe('SocialComponent', () => {
-  let component: SocialComponent;
-  let fixture: ComponentFixture<SocialComponent>;
+describe('HardComponent', () => {
+  let component: HardComponent;
+  let fixture: ComponentFixture<HardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
+      declarations: [ HardComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialComponent);
+    fixture = TestBed.createComponent(HardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
